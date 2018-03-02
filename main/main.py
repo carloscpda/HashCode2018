087c52b107a8e51624d19dbbeb572e17586f64c4
cepeda <carlos@cepeda.me>
2018-03-02 18:02:29 +0100

    Forty-three millions

--- main/main.py
+++ main/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # constants
 a = 0  # The row of the start intersection
@@ -8,10 +9,6 @@ y = 3  # The column of the finish intersection
 s = 4  # The earliest start
 f = 5  # The latest finish
 c = 2  # Steps to be available
-
-example = "a_example"
-should_be_easy = "b_should_be_easy"
-metropolis = "d_metropolis"
 
 # global variables
 data = n_rows = n_cols = n_cars = n_rides = bonus = t_steps = 0
@@ -55,14 +52,14 @@ def ride_distance(a, b, x, y):
     return abs(y - b) + abs(x - a)
 
 
-def main():
-    exercise = example
+def loop(exercise):
     parser_input(exercise)
     cars = [[0 for _ in range(3)] for _ in range(n_cars)]
     output = [[0] for _ in range(n_cars)]
-    max_distance = ride_distance(0, 0, n_rows, n_cols)
 
     for a_step in range(0, t_steps):
+        for percent_step in range(10):
+            if a_step == math.ceil(t_steps / 10) * percent_step: print("=", end='')
 
         for idx_car, car in enumerate(cars):
             chosen_ride = -1
@@ -71,29 +68,38 @@ def main():
 
             # Elegimos coche vacio
             if car[c] == 0:
+                min_steps = t_steps
                 for idx_ride, ride in enumerate(data):
                     travel_distance = ride_distance(ride[a], ride[b], ride[x], ride[y])
-                    car_to_ride_distance = max_distance
+                    car_to_ride_distance = ride_distance(car[a], car[b], ride[a], ride[b])
+                    total_distance = travel_distance + car_to_ride_distance
 
-                    if ride_distance(car[a], car[b], ride[a], ride[b]) <= car_to_ride_distance:
-                        car_to_ride_distance = min(car_to_ride_distance,
-                                                   ride_distance(car[a], car[b], ride[a], ride[b]))
-                        total_distance = travel_distance + car_to_ride_distance
+                    if total_distance <= ride[f] - a_step and total_distance <= ride[f] - ride[s]:
+                        wait_time = ride[s] - a_step
+                        if wait_time - car_to_ride_distance > 0:
+                            total_distance += wait_time - car_to_ride_distance
 
-                        if total_distance <= ride[f] - a_step and total_distance <= ride[f] - ride[s]:
+                        if total_distance <= min_steps:
+                            min_steps = total_distance
                             chosen_ride = idx_ride
 
                 if chosen_ride != -1:
-                    car[c] = total_distance
-                    if data[chosen_ride][s] - a_step - car_to_ride_distance > 0:
-                        car[c] += data[chosen_ride][s] - a_step - car_to_ride_distance
+                    car[c] = min_steps
                     car[a] = data[chosen_ride][x]
                     car[b] = data[chosen_ride][y]
                     data[chosen_ride][f] = 0
                     output[idx_car].append(chosen_ride)
                     output[idx_car][0] += 1
 
+    print("\n")
     save_output(exercise, output)
+
+
+def main():
+    for exercise in ["a_example", "b_should_be_easy", "c_no_hurry", "d_metropolis", "e_high_bonus"]:
+        print("Start exercise ", exercise)
+        loop(exercise)
+        print("Finish exercise ", exercise)
 
 
 if __name__ == "__main__":
