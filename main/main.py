55e75b31506da3bfb2cf8c6ecad783ba86584da2
cepeda <carlos@cepeda.me>
2018-03-02 18:36:16 +0100

    Max and not min

--- main/main.py
+++ main/main.py
@@ -9,6 +9,7 @@ y = 3  # The column of the finish intersection
 s = 4  # The earliest start
 f = 5  # The latest finish
 c = 2  # Steps to be available
+ride_done = -1
 
 # global variables
 data = n_rows = n_cols = n_cars = n_rides = bonus = t_steps = 0
@@ -52,14 +53,14 @@ def ride_distance(a, b, x, y):
     return abs(y - b) + abs(x - a)
 
 
+# TODO Si llega justo a tiempo no da puntos a no ser que si de el bonus ¿?¿?
 def loop(exercise):
     parser_input(exercise)
     cars = [[0 for _ in range(3)] for _ in range(n_cars)]
     output = [[0] for _ in range(n_cars)]
 
     for a_step in range(0, t_steps):
-        for percent_step in range(10):
-            if a_step == math.ceil(t_steps / 10) * percent_step: print("=", end='')
+        print(a_step)
 
         for idx_car, car in enumerate(cars):
             chosen_ride = -1
@@ -68,25 +69,36 @@ def loop(exercise):
 
             # Elegimos coche vacio
             if car[c] == 0:
-                min_steps = t_steps
+                points = 0
+                steps = t_steps
                 for idx_ride, ride in enumerate(data):
+                    if ride[s] == -1:
+                        continue
                     travel_distance = ride_distance(ride[a], ride[b], ride[x], ride[y])
+                    a_points = travel_distance
                     car_to_ride_distance = ride_distance(car[a], car[b], ride[a], ride[b])
                     total_distance = travel_distance + car_to_ride_distance
 
                     if total_distance <= ride[f] - a_step and total_distance <= ride[f] - ride[s]:
                         wait_time = ride[s] - a_step
+
                         if wait_time - car_to_ride_distance > 0:
                             total_distance += wait_time - car_to_ride_distance
 
-                        if total_distance <= min_steps:
-                            min_steps = total_distance
+                        a_points -= wait_time
+                        if car_to_ride_distance + a_step == ride[s]:
+                            a_points += bonus
+
+                        if a_points >= points:
                             chosen_ride = idx_ride
+                            points = a_points
+                            steps = total_distance
 
                 if chosen_ride != -1:
-                    car[c] = min_steps
+                    car[c] = steps
                     car[a] = data[chosen_ride][x]
                     car[b] = data[chosen_ride][y]
+                    data[chosen_ride][s] = ride_done
                     data[chosen_ride][f] = 0
                     output[idx_car].append(chosen_ride)
                     output[idx_car][0] += 1
