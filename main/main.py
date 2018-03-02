cc3921d1ea86a1b0bb2467c0a2666b4a62b76933
cepeda <carlos@cepeda.me>
2018-03-02 10:57:56 +0100

    Refactor input and output code

--- main/main.py
+++ main/main.py
@@ -1,20 +1,33 @@
 import numpy as np
 
-a = 0   # The row of the start intersection
-b = 1   # The column of the start intersection
-x = 2   # The row of the finish intersection
-y = 3   # The column of the finish intersection
-s = 4   # The earliest start
-f = 5   # The latest finish
-c = 2   # Contador
+# constants
+a = 0  # The row of the start intersection
+b = 1  # The column of the start intersection
+x = 2  # The row of the finish intersection
+y = 3  # The column of the finish intersection
+s = 4  # The earliest start
+f = 5  # The latest finish
+c = 2  # Steps to be available
 
-output_file = "../data/output.out"
+example = "a_example"
+should_be_easy = "b_should_be_easy"
+metropolis = "d_metropolis"
 
-def main():
-    # PARSER
-    header = np.loadtxt("../data/a_example_header.in", dtype=int)
-    data = np.loadtxt("../data/a_example.in", dtype=int)
+# global variables
+data = n_rows = n_cols = n_cars = n_rides = bonus = t_steps = 0
 
+
+def parser_input(exercise):
+    input_file = "../data/" + exercise + ".in"
+    with open(input_file, 'r') as input_file_str:
+        header_str = input_file_str.readline().replace('\n', '')
+
+    input_file_str.close()
+
+    header = np.fromstring(header_str, sep=' ', dtype=int)
+
+    global data, n_rows, n_cols, n_cars, n_rides, bonus, t_steps
+    data = np.loadtxt(input_file, dtype=int, skiprows=1)
     n_rows = header[0]
     n_cols = header[1]
     n_cars = header[2]
@@ -22,21 +35,39 @@ def main():
     bonus = header[4]
     t_steps = header[5]
 
-    cars = [[0 for z in range(3)] for z2 in range(n_cars)]
+
+def save_output(exercise, output):
+    output_file_path = "../data/" + exercise + ".out"
+    output_file = open(output_file_path, 'w')
+    output_file.truncate()
+    for out in output:
+        for idx, char in enumerate(out):
+            if idx == 0:
+                output_file.write(str(char))
+            else:
+                output_file.write(" " + str(char))
+
+        output_file.write("\n")
+    output_file.close()
+
+
+def ride_distance(a, b, x, y):
+    return abs(y - b) + abs(x - a)
+
+
+def main():
+    exercise = example
+    parser_input(exercise)
+    cars = [[0 for _ in range(3)] for _ in range(n_cars)]
+    output = [[0] for _ in range(n_cars)]
     max_distance = ride_distance(0, 0, n_rows, n_cols)
 
-    output = [[0] for z2 in range(n_cars)]
-
-    a_step = 0
-
     for a_step in range(0, t_steps):
-        print('Actual step is', a_step)
 
         for idx_car, car in enumerate(cars):
             chosen_ride = -1
             if car[c] is not 0:
                 car[c] = car[c] - 1
-
 
             # Elegimos coche vacio
             if car[c] == 0:
@@ -45,12 +76,12 @@ def main():
                     car_to_ride_distance = max_distance
 
                     if ride_distance(car[a], car[b], ride[a], ride[b]) <= car_to_ride_distance:
-                        car_to_ride_distance = min(car_to_ride_distance, ride_distance(car[a], car[b], ride[a], ride[b]))
+                        car_to_ride_distance = min(car_to_ride_distance,
+                                                   ride_distance(car[a], car[b], ride[a], ride[b]))
                         total_distance = travel_distance + car_to_ride_distance
 
                         if total_distance <= ride[f] - a_step and total_distance <= ride[f] - ride[s]:
                             chosen_ride = idx_ride
-
 
                 if chosen_ride != -1:
                     car[c] = total_distance
@@ -59,24 +90,11 @@ def main():
                     car[a] = data[chosen_ride][x]
                     car[b] = data[chosen_ride][y]
                     data[chosen_ride][f] = 0
-                    #print("Ride:", chosen_ride, "Car:", idx_car)
                     output[idx_car].append(chosen_ride)
                     output[idx_car][0] += 1
 
-    for out in output:
-        for idx, char in enumerate(out):
-            with open(output_file, "a") as my_file:
-                if idx == 0:
-                    my_file.write(str(char))
-                else:
-                    my_file.write(" " + str(char))
+    save_output(exercise, output)
 
-        with open(output_file, "a") as my_file:
-            my_file.write("\n")
-
-
-def ride_distance(a, b, x, y):
-    return abs(y-b)+abs(x-a)
 
 if __name__ == "__main__":
     main()
